8b9632dc9bff574eac31bf1b8032b526e7d6e8c2
Michael Hans <49339028+michaellhans@users.noreply.github.com>
2019-09-20 23:11:34 +0700

    Add files via upload

--- MainMatriks.java
+++ MainMatriks.java
@@ -10,47 +10,48 @@ import java.util.Scanner;
 
 public class MainMatriks{
     public static void main(String []args) {
-        /* Kamus Lokal */
+        /* KAMUS UTAMA */
         MatriksAdt obj = new MatriksAdt();
-        int i,j;
-        MatriksAdt.MATRIKS M1 = new MatriksAdt.MATRIKS();
-        MatriksAdt.MATRIKS M2 = new MatriksAdt.MATRIKS();
-        MatriksAdt.MATRIKS M3 = new MatriksAdt.MATRIKS();
-        MatriksAdt.MATRIKS M4 = new MatriksAdt.MATRIKS(); 
-        /* Algoritma */
-        System.out.println("Program Invers Matriks");
-        System.out.println("Matriks M1 :");
-        obj.BacaMATRIKS(M1);
+        int op = 1;
+        Scanner keyboard = new Scanner (System.in);
+        /* AlGORITMA */
         System.out.println();
-        System.out.println("Matriks Kanan :");
-        obj.BacaMATRIKS(M3);
+        System.out.println("IF2123 Aljabar Linear dan Geometri");
+        System.out.println("Sistem Persamaan Linear, Determinan, dan Aplikasinya");
         System.out.println();
-        System.out.println("Determinan M1 adalah "+obj.Determinan(M1));
+        System.out.println("Created by : Hans-Demy");
+        System.out.println("13518056 - Michael Hans");
+        System.out.println("13518119 - Lionnarta Savirandy");
+        System.out.println("13518128 - Rafael Sean Putra");
         System.out.println();
-        System.out.println("Matriks Kofaktor dari Matriks M1 adalah :");
-        M2 = obj.MatriksKofaktor(M1);
-        obj.TulisMATRIKS(M2);
-        System.out.println();
-        System.out.println();
-        System.out.println("Adjoin dari Matriks M1 adalah :");
-        M2 = obj.Adjoin(M1);
-        obj.TulisMATRIKS(M2);
-        System.out.println();
-        System.out.println();
-        if (obj.Determinan(M1)!=0){
-            M2 = obj.Invers(M1);
-            System.out.println("Invers dari Matriks M1 adalah :");
-            obj.TulisMATRIKS(M2);
+        while ((op > 0) & (op < 8)){
+            System.out.println("MENU PROGRAM");
+            System.out.println("1. Sistem Persamaan Linier");
+            System.out.println("2. Determinan");
+            System.out.println("3. Matriks Balikan");
+            System.out.println("4. Matriks Kofaktor");
+            System.out.println("5. Adjoin");
+            System.out.println("6. Interpolasi Polinom");
+            System.out.println("7. Keluar");
+            System.out.print("Masukkan pilihan menu yang diinginkan : ");
+            op = keyboard.nextInt();
             System.out.println();
-        }
-        else {
-            System.out.println("M1 tidak mempunyai invers");
-        }
-        System.out.println();
-        System.out.println("Solusi persamaan Ax = B adalah ");
-        M4 = obj.KaliMATRIKS(M2, M3);
-        for (i=1; i<=M1.NBrsEff; i++){
-            System.out.println("x"+i+" = "+String.format("%.4f", M4.Mem[i][1]));
+            switch(op){
+                case 1:
+                    obj.SPL(); break;
+                case 2:
+                    obj.MainDeterminan(); break;
+                case 3:
+                    obj.MainInvers(); break;
+                case 4:
+                    obj.MainKofaktor(); break;
+                case 5:
+                    obj.MainAdjoin(); break;
+                case 6:
+                    obj.Interpolasi(); break;
+                case 7:
+                    obj.ExitProgram();
+            }
         }
     }
 }
